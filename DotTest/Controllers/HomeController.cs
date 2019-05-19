5985c6521d10a99d5d499cfe5e517bce9b5028fc
xmgChauncey <1619112212@qq.com>
2019-05-19 15:51:20 +0800

    First submit

---
+++ DotTest/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using DotTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DotTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+        public string Add()
+        {
+            return "Hello world!";
+        }
+    }
+}
---
+++ DotTest/Models/GuestResponse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotTest.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage ="please enter your name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage ="please enter your email address")]
+        [RegularExpression(".+\\@.+\\..+",
+                ErrorMessage="Please enter a valid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage ="Please enter your phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage ="Please specify whether you'll attend")]
+        public bool ? WillAttend { get; set; }
+
+    }
+}
